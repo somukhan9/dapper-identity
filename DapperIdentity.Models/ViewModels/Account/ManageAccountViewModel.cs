fe56b22208ac741ec2c79deb235f94913111db00
somukhan <mhsomrat154@gmail.com>
2025-06-25 01:26:22 +0600

    complete Email and ChanagePassword under ManageAccountController

--- DapperIdentity.Models/ViewModels/Account/ManageAccountViewModel.cs
+++ DapperIdentity.Models/ViewModels/Account/ManageAccountViewModel.cs
@@ -2,11 +2,11 @@
 
 namespace DapperIdentity.Models.ViewModels.Account;
 
-public class ManageAccountViewModel
+public class ProfileViewModel
 {
     [Required]
     [Display(Name = "User Name")]
-    public string Username;
+    public string? Username { get; set; }
 
     [Display(Name = "First Name")]
     public string? FirstName { get; set; }
@@ -16,7 +16,42 @@ public class ManageAccountViewModel
 
     [Phone]
     [Display(Name = "Phone number")]
+    [DataType(DataType.PhoneNumber)]
     public string? PhoneNumber { get; set; }
+}
 
-    public string? StatusMessage { get; set; }
+public class EmailViewModel
+{
+    [Required]
+    [Display(Name = "Email Address")]
+    [DataType(DataType.EmailAddress)]
+    public string? Email { get; set; }
+
+    [Required]
+    [Display(Name = "New Email Address")]
+    [DataType(DataType.EmailAddress)]
+    public string? NewEmail { get; set; }
+
+    [Display(Name = "Email Confirmation")]
+    public bool IsEmailConfirmed { get; set; }
+}
+
+public class ChangePasswordViewModel
+{
+    [Required]
+    [Display(Name = "Current password")]
+    [DataType(DataType.Password)]
+    public string? OldPassword { get; set; }
+
+    [Required]
+    [Display(Name = "New Password")]
+    [DataType(DataType.Password)]
+    [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+    public string? NewPassword { get; set; }
+
+    [Required]
+    [Display(Name = "Confirm Password")]
+    [DataType(DataType.Password)]
+    [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+    public string? ConfirmPassword { get; set; }
 }
--- DapperIdentity.Web/Areas/Identity/Controllers/ManageAccountController.cs
+++ DapperIdentity.Web/Areas/Identity/Controllers/ManageAccountController.cs
@@ -1,7 +1,11 @@
 ﻿using DapperIdentity.Models.Identity;
 using DapperIdentity.Models.ViewModels.Account;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebUtilities;
+using System.Text;
+using System.Text.Encodings.Web;
 
 namespace DapperIdentity.Web.Areas.Identity.Controllers;
 
@@ -9,72 +13,177 @@ namespace DapperIdentity.Web.Areas.Identity.Controllers;
 public class ManageAccountController(
     UserManager<ApplicationUser> userManager,
     SignInManager<ApplicationUser> signInManager,
-    IUserStore<ApplicationUser> userStore)
+    IUserStore<ApplicationUser> userStore,
+    IEmailSender emailSender,
+    ILogger<ManageAccountController> logger)
     : Controller
 {
     public async Task<IActionResult> Index()
     {
         var user = await userManager.GetUserAsync(User);
-        if (user == null)
+
+        if (user is null)
         {
             return NotFound($"Unable to load user with ID '{userManager.GetUserId(User)}'.");
         }
 
-        var manageAccVM = await LoadAsync(user);
-        return View("Index", manageAccVM);
+        var vm = new ProfileViewModel()
+        {
+            Username = user.UserName!,
+            FirstName = user.FirstName,
+            LastName = user.LastName,
+            PhoneNumber = user.PhoneNumber
+        };
+        return View("Index", vm);
     }
 
     [HttpPost]
-    public async Task<IActionResult> Index(ManageAccountViewModel manageAccVM, CancellationToken cancellationToken)
+    public async Task<IActionResult> Index(ProfileViewModel vm, CancellationToken cancellationToken)
     {
         var user = await userManager.GetUserAsync(User);
-        if (user == null)
+
+        if (user is null)
+        {
+            return NotFound($"Unable to load user with ID '{userManager.GetUserId(User)}'.");
+        }
+
+        var shouldUpdate = false;
+
+        if (!string.IsNullOrEmpty(vm.PhoneNumber) && !vm.PhoneNumber.Equals(user.PhoneNumber))
+        {
+            user.PhoneNumber = vm.PhoneNumber;
+            shouldUpdate = true;
+        }
+
+        if (!string.IsNullOrEmpty(vm.FirstName) && !vm.FirstName.Equals(user.FirstName))
+        {
+            user.FirstName = vm.FirstName;
+            shouldUpdate = true;
+        }
+
+        if (!string.IsNullOrEmpty(vm.LastName) && !vm.LastName.Equals(user.LastName))
+        {
+            user.LastName = vm.LastName;
+            shouldUpdate = true;
+        }
+
+        if (ModelState.IsValid)
+        {
+            if (shouldUpdate)
+            {
+                await userStore.UpdateAsync(user, cancellationToken);
+                ViewBag.StatusMessage = "Your profile has been updated";
+            }
+
+            await signInManager.RefreshSignInAsync(user);
+        }
+
+        return View(vm);
+    }
+
+    public async Task<IActionResult> Email()
+    {
+        var user = await userManager.GetUserAsync(User);
+
+        if (user is null)
+        {
+            return NotFound($"Unable to load user with ID '{userManager.GetUserId(User)}'.");
+        }
+
+        var vm = new EmailViewModel()
+        {
+            Email = user.Email!,
+            NewEmail = user.Email!,
+            IsEmailConfirmed = await userManager.IsEmailConfirmedAsync(user)
+        };
+
+        return View(vm);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Email(EmailViewModel vm)
+    {
+        var user = await userManager.GetUserAsync(User);
+
+        if (user is null)
         {
             return NotFound($"Unable to load user with ID '{userManager.GetUserId(User)}'.");
         }
 
         if (!ModelState.IsValid)
         {
-            manageAccVM = await LoadAsync(user);
-            return View("Index", manageAccVM);
+            return View(vm);
         }
 
-        var shouldUpdate = false;
-
-        if (!string.IsNullOrEmpty(manageAccVM.PhoneNumber) && !manageAccVM.PhoneNumber.Equals(user.PhoneNumber))
+        var email = await userManager.GetEmailAsync(user);
+        if (!string.IsNullOrEmpty(vm.NewEmail) && vm.NewEmail != email)
         {
-            user.PhoneNumber = manageAccVM.PhoneNumber;
-            shouldUpdate = true;
+            var userId = await userManager.GetUserIdAsync(user);
+            var code = await userManager.GenerateChangeEmailTokenAsync(user, vm.NewEmail);
+            code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+            var callbackUrl = Url.Page(
+                "/Account/ConfirmEmailChange",
+                pageHandler: null,
+                values: new { area = "Identity", userId = userId, email = vm.NewEmail, code = code },
+                protocol: Request.Scheme);
+            await emailSender.SendEmailAsync(
+                vm.NewEmail,
+                "Confirm your email",
+                $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+
+            ViewBag.StatusMessage = "Confirmation link to change email sent. Please check your email.";
+            //await userManager.SetEmailAsync(user, vm.NewEmail);
+            return View(vm);
         }
 
-        if (!string.IsNullOrEmpty(manageAccVM.FirstName) && !manageAccVM.FirstName.Equals(user.FirstName))
+        ViewBag.StatusMessage = "Your email is unchanged.";
+        return View(vm);
+    }
+
+    public IActionResult ChangePassword()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> ChangePassword(ChangePasswordViewModel vm)
+    {
+        if (!ModelState.IsValid)
         {
-            user.FirstName = manageAccVM.FirstName;
-            shouldUpdate = true;
+            return View(vm);
         }
 
-        if (!string.IsNullOrEmpty(manageAccVM.LastName) && !manageAccVM.LastName.Equals(user.LastName))
+        var user = await userManager.GetUserAsync(User);
+
+        if (user is null)
         {
-            user.LastName = manageAccVM.LastName;
-            shouldUpdate = true;
+            return NotFound($"Unable to load user with ID '{userManager.GetUserId(User)}'.");
         }
 
-        if (shouldUpdate)
+        if (!ModelState.IsValid)
         {
-            await userStore.UpdateAsync(user, cancellationToken);
+            return View();
+        }
+
+        if (string.IsNullOrEmpty(vm.OldPassword) || string.IsNullOrEmpty(vm.NewPassword))
+        {
+            ViewBag.StatusMessage = "Must provide a new password to change password and also confirm it.";
+            return View();
+        }
+
+        var changePasswordResult = await userManager.ChangePasswordAsync(user, vm.OldPassword, vm.NewPassword);
+        if (!changePasswordResult.Succeeded)
+        {
+            foreach (var error in changePasswordResult.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+            return View();
         }
 
         await signInManager.RefreshSignInAsync(user);
-        manageAccVM.StatusMessage = "Your profile has been updated";
-        return RedirectToAction(nameof(Index));
+        logger.LogInformation("User changed their password successfully.");
+        ViewBag.StatusMessage = "Your password has been changed.";
+        return View();
     }
-
-    #region Private Methods Specific to Controller
-    private async Task<ManageAccountViewModel> LoadAsync(ApplicationUser user)
-    {
-        var manageAccVM = new ManageAccountViewModel() { Username = user.UserName!, PhoneNumber = user.PhoneNumber, FirstName = user.FirstName, LastName = user.LastName };
-
-        return manageAccVM;
-    }
-    #endregion
 }
