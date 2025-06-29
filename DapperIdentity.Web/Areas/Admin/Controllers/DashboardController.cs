0555dde204093bbae20a15167e6d45aa61914166
somukhan <mhsomrat154@gmail.com>
2025-06-30 02:24:10 +0600

    Added some files

---
+++ DapperIdentity.Web/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,15 @@
+﻿using Common.Constants;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DapperIdentity.Web.Areas.Admin.Controllers;
+
+[Area(areaName: "Admin")]
+[Authorize(Roles = SD.ROLE_ADMIN)]
+public class DashboardController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
